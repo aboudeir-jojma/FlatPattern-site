fd300c43c1c63413446be90ba3c5d773f2b061cb
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-10-13 12:39:50 +0100

    proxy path config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   trailingSlash: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://192.168.100.134:5000/:path*', // ton backend Flask local
+      },
+    ];
+  },
 };
 
 export default nextConfig;
