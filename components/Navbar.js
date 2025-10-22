bd87038752c845ce933c98064b5f6257d5b9cde9
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-10-22 13:06:36 +0100

    message resultat elimier

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,15 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   const [active, setActive] = useState("/");
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    const updateActive = () => {
-      const { pathname, hash } = window.location;
-      if (hash) {
-        setActive(hash);
-      } else {
-        setActive(pathname || "/");
-      }
-    };
-    updateActive();
-    window.addEventListener("hashchange", updateActive);
-    return () => window.removeEventListener("hashchange", updateActive);
-  }, []);
+    setActive(pathname);
+  }, [pathname]);
 
   const handleLinkClick = (href) => {
     setActive(href);
