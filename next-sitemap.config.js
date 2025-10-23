ae722e0de5a6a49f7d53c6e26955b5db009bd7b2
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-10-23 11:34:13 +0100

    sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,21 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://sheetmetaldevelopment.com',
   generateRobotsTxt: true,
-  sitemapSize: 7000,
+  sitemapSize: 5000,
+  outDir: './public',
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/404', '/server-sitemap-index.xml'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'https://sheetmetaldevelopment.com/sitemap-0.xml',
+    ],
+  },
 };
